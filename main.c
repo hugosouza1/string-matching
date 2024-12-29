490f9e8ff712cf529ae7c2f184dc5797a84721d3
hugostoso1 <hugoaasouza45@gmail.com>
2024-12-29 13:32:37 -0300

    gatinha

--- main.c
+++ main.c
@@ -15,24 +15,31 @@ int main(int *arg, char *argv[]){
     int x = atoi(argv[2]);
     switch(x){
         case 1:
+            printf("Forca Bruta:\n");
             resolucaoForcaBruta(fila);
             break;
         case 2:
+            printf("KMP:\n");
             resolucaoKMP(fila);
             break;
         case 3:
+            printf("BMH:\n");
             resolucaoBMH(fila);
             break;
         case 4:
             //resolucaoShiftAnd(fila);
             break;
         case 5:
+            printf("Forca Bruta:\n");
             resolucaoForcaBruta(fila);
             printf("\n");
+            printf("KMP:\n");
             resolucaoKMP(fila);
             printf("\n");
+            printf("BMH:\n");
             resolucaoBMH(fila);
             //printf("\n");
+            //printf("ShiftAnd:\n");
             //resolucaoShiftAnd(fila);
             break;
     }
