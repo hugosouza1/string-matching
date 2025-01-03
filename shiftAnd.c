b3f3c0f64d694168f3d769cec363a4c624d3ace2
hugostoso1 <hugoaasouza45@gmail.com>
2025-01-02 23:27:44 -0300

    junta

--- shiftAnd.c
+++ shiftAnd.c
@@ -8,9 +8,16 @@ int *criarMascara(NO* nota) {
         mascara[i] = 0;
     }
     for(int i = 0; i < nota->plagioTamanho; i++) {
-        mascara[nota->plagio[i]] |= (1 << i); // Cria a máscara
+        mascara[nota->plagio[i]] |= (1 << i ); // Cria a máscara
     } // Cada posição do vetor máscara vai ter um bit setado para 1, representando a ocorrência de um elemento da string
     return mascara;
+}
+
+void printBits(int num, int tamanho) {
+    for(int i = tamanho - 1; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
+    printf("\n");
 }
 
 void shiftAnd(NO* nota) {
@@ -21,9 +28,19 @@ void shiftAnd(NO* nota) {
 
     int r = 0;
 
+    printf("---------------------------------------------------------\n");
+
     for(int i = 0; i < nota->originalTamanho; i++) {
         r = ((r >> 1) | (1 << (nota->plagioTamanho - 1))) & mascara[original[i]];
-        if(r & (1 << (nota->plagioTamanho - 1)) != 0) {
+        
+   //     printf("i: %d, original[i]: %d, r: ", i, original[i]);
+/*         printBits(r, nota->plagioTamanho);
+        printf("mascara[original[i]]: ");
+        printBits(mascara[original[i]], nota->plagioTamanho);
+        printf("\n");
+ */
+
+        if((r & 1) == 1) {
             printf("Plagio encontrado na posicao %d\n", i - nota->plagioTamanho + 1);
             free(mascara);
             return;
