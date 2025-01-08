02a903bcec563c858875927763dbb95b9ef3e07d
hugostoso1 <hugoaasouza45@gmail.com>
2025-01-08 00:04:21 -0300

    n testei

--- leitura.c
+++ leitura.c
@@ -3,19 +3,19 @@
 int *identificaTecla(double *melodia, int melodiaTamanho){
     int *teclas = (int*)malloc(melodiaTamanho * sizeof(int));
     for(int i = 0; i < melodiaTamanho; i++){
-        if(melodia[i] == 64.5)      teclas[i] = 12; //Ab
-        else if(melodia[i] == 65)   teclas[i] = 1; // A
-        else if(melodia[i] == 65.5) teclas[i] = 2; // A#
-        else if(melodia[i] == 66)   teclas[i] = 3; // B
-        else if(melodia[i] == 67)   teclas[i] = 4; // C
-        else if(melodia[i] == 67.5) teclas[i] = 5; //C#
-        else if(melodia[i] == 68)   teclas[i] = 6; // D
-        else if(melodia[i] == 68.5) teclas[i] = 7; //D#
-        else if(melodia[i] == 69)   teclas[i] = 8; // E
-        else if(melodia[i] == 70)   teclas[i] = 9; // F
-        else if(melodia[i] == 70.5) teclas[i] = 10; // F#
-        else if(melodia[i] == 71)   teclas[i] = 11; // G
-        else if(melodia[i] == 71.5) teclas[i] = 12; // G#
+        if(melodia[i] == 64.8)                             teclas[i] = 12; //Ab
+        else if(melodia[i] == 65)                          teclas[i] = 1; // A
+        else if(melodia[i] == 65.2 || melodia[i] == 65.8)                teclas[i] = 2; // A#
+        else if(melodia[i] == 66   || melodia[i] == 66.8)  teclas[i] = 3; // B
+        else if(melodia[i] == 67   || melodia[i] == 66.2)  teclas[i] = 4; // C
+        else if(melodia[i] == 67.2 || melodia[i] == 67.8)  teclas[i] = 5; //C#
+        else if(melodia[i] == 68)                          teclas[i] = 6; // D
+        else if(melodia[i] == 68.2 || melodia[i] == 68.8)  teclas[i] = 7; //D#
+        else if(melodia[i] == 69   || melodia[i] == 69.8)  teclas[i] = 8; // E
+        else if(melodia[i] == 70   || melodia[i] == 69.2)  teclas[i] = 9; // F
+        else if(melodia[i] == 70.2 || melodia[i] == 70.8 ) teclas[i] = 10; // F#
+        else if(melodia[i] == 71)                          teclas[i] = 11; // G
+        else if(melodia[i] == 71.2)                        teclas[i] = 12; // G#
     }
     return teclas;
 }
@@ -25,10 +25,10 @@ int *converteMelodia(char *linha, double *array, int arrayTamanho) {
     for(int i = 0; token != NULL && i < arrayTamanho; i++) {
         if(token[1] == '#'){
             double temp = token[0] ;
-            array[i] = temp + 0.5;
+            array[i] = temp + 0.2;
         } else if(token[1] == 'b'){
             double temp = token[0] ;
-            array[i] = temp - 0.5;
+            array[i] = temp - 0.2;
         } else {
             array[i] = token[0];
         }
