be1c63d680f3f33b1a527c8662525371f9f17c51
Rafael Tavares <rafatavares03@hotmail.com>
2025-01-08 12:04:24 -0300

    e la vamos nos

--- shiftAnd.c
+++ shiftAnd.c
@@ -7,80 +7,73 @@ void printBits(int num, int tamanho) {
     printf("\n");
 }
 
-int *criarMascara(int *padrao, int padraoTamanho) {
-    int tamanhoAlfabeto = 13;
+int tomShift(int valor, int tom) {
+    valor += tom;
+    if(valor > 12) {
+        valor -= 12;
+    }
+    return valor;
+}
+
+int *criarMascara(NO *nota) {
+    int tamanhoAlfabeto = 12;
     int *mascara = (int*)malloc(tamanhoAlfabeto * sizeof(int));
     for(int i = 0; i < tamanhoAlfabeto; i++){
         mascara[i] = 0;
     }
-    for(int i = 0; i < padraoTamanho; i++){
-        mascara[padrao[i]] = mascara[padrao[i]] | ((1 << (padraoTamanho - 1)) >> i);
+    for(int i = 0; i < nota->plagioTamanho; i++){
+        mascara[nota->plagio[i]-1] = mascara[nota->plagio[i]-1] | ((1 << (nota->plagioTamanho - 1)) >> i);
     }
     return mascara;
-}
-
-int *copiaVetor(int *vet, int tam){
-    int *novo = (int*) malloc (sizeof(int) * tam);
-    for(int i = 0; i < tam; i++){
-        novo[i] = vet[i];
-    }
-    return novo;
-}
-
-void incrementarVetor(int *vetor, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        vetor[i]++;
-        if (vetor[i] > 12) {
-            vetor[i] = 1; // Ajusta para o intervalo de 1 a 12
-        }
-    }
 }
 
 void shiftAnd(NO *nota){
     int *original = nota->original;
     int *plagio = nota->plagio;
-    int *copiaPlagio = copiaVetor(plagio, nota->plagioTamanho);
-
-    int **mascaras = (int**) malloc(sizeof(int*) * 13);
-
-    for(int i = 0; i < 13; i++){
-        mascaras[i] = criarMascara(copiaPlagio, nota->plagioTamanho);
-        incrementarVetor(copiaPlagio, nota->plagioTamanho);
+    int *mascara = criarMascara(nota);
+    /*
+    for(int i = 0; i < nota->originalTamanho; i++) printf("%d ", original[i]);
+    printf("\n");
+    for(int i = 0; i < nota->plagioTamanho; i++) printf("%d ", plagio[i]);
+    printf("\n");
+    */
+    /*
+    for(int i = 0; i < 12; i++) {
+        printf("mascara[%d]: ", i);
+        printBits(mascara[i], nota->plagioTamanho); 
     }
-
-    free(copiaPlagio);
+    */
     
-    int *r = (int*) malloc (sizeof(int) * 13);
-
-    for(int i = 0; i < 13; i++) r[i] = 0;
-
-/*     for(int i = 0; i < 12; i++){
-        printBits(mascara[i], 12);
-    } */
-
-    for(int i = 0; i < nota->originalTamanho; i++) {
-        for(int j = 0; j < 13; j++) {
-
-            r[j] = ((r[j] >> 1) | (1 << (nota->plagioTamanho - 1))) & mascaras[j][original[i]];
-            
-            if(r[j] & 1 != 0) {
-                printf("S %d\n", i - nota->plagioTamanho + 1);
-                for(int k = 0; k < 13; k++) {
-                    free(mascaras[k]);
-                }
-                free(mascaras);
-                free(r);
-                return;
+    int r = 0;
+    int tom = tons(original[0], plagio[0]), k = 0;
+    //printf("tons: ");
+    for(int i = 0; i < nota->originalTamanho; i++){
+        if(tomShift(original[i], tom) != plagio[k]) {
+            //printf("deu errado: %d %d %d\n", tomShift(original[i], tom), plagio[k], original[i]);
+            if(r != 0 && i > 0) {
+                if(original[i] == original[i-1]) continue;
             }
+            k = r = 0;
+            tom = tons(original[i], plagio[k]);
         }
+        //printf("%d ",tom);
+        r = ((r >> 1) | (1 << (nota->plagioTamanho - 1))) & mascara[tomShift(original[i], tom)-1];
+/*
+        printf("i: %d, original[i]: %d, r: ", i, original[i]);
+        printBits(r, nota->plagioTamanho);
+        printf("mascara[original[i]]: ");
+        printBits(mascara[original[i]], nota->plagioTamanho);
+        printf("\n");
+*/      
+        if((r & 1) == 1){
+            printf("S %d\n", i - nota->plagioTamanho + 1);
+            free(mascara);
+            return;
+        }
+        k++;
     }
-
     printf("N\n");
-    for(int i = 0; i < 13; i++) {
-        free(mascaras[i]);
-    }
-    free(mascaras);
-    free(r);
+    free(mascara);
 }
 
 void resolucaoShiftAnd(Fila *notas) {
