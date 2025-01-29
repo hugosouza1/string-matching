a92699cb086943f7e48e0b9c265d25f2ce956981
Hugo Souza <136270146+hugostoso1@users.noreply.github.com>
2025-01-29 13:30:27 -0300

    babado

--- main.c
+++ main.c
@@ -54,3 +54,56 @@ int main(int *arg, char *argv[]){
     
     return 0;
 }
+
+
+void geraAruivo(){
+   srand(time(NULL));
+
+    FILE *teste = fopen("teste.txt", "w");
+
+    for (int i = 0; i < 15; i++) {
+
+        int x = 40 + (rand() % 100);
+        int *vet = (int*) malloc(sizeof(int) * x);
+        if (vet == NULL) {
+            printf("Erro ao alocar memória para vet\n");
+            return 1;
+        }
+
+        for (int j = 0; j < x; j++) {
+            vet[j] = 65 + (rand() % 6);
+        }
+
+        int ini = rand() % (x / 2);
+        int fifi = ini +1+ (rand() % (x / 2));
+        if (fifi > x) fifi = x;
+
+        int *vet2 = (int*) malloc(sizeof(int) * (fifi - ini));
+        if (vet2 == NULL) {
+            printf("Erro ao alocar memória para vet2\n");
+            free(vet);
+            return 1;
+        }
+
+        for (int k = ini, j = 0; k < fifi; k++, j++) {
+            vet2[j] = vet[k];
+        }
+
+        fprintf(teste, "%d %d\n", x, (fifi - ini));
+        for (int ii = 0; ii < x; ii++) {
+            fprintf(teste, "%c ", vet[ii]);
+        }
+        fprintf(teste, "\n");
+
+        for (int iii = 0; iii < fifi - ini; iii++) {
+            fprintf(teste, "%c ", vet2[iii]);
+        }
+        fprintf(teste, "\n");
+
+        free(vet);
+        free(vet2);
+    }
+
+    fclose(teste); 
+
+}
